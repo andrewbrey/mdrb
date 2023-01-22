c98326310a93eec99a7d6490040e3627d63da825
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-22 15:56:09 -0800

    chore: add debug logging

--- mod.test.ts
+++ mod.test.ts
@@ -6,8 +6,18 @@ const demo = $.relativePath(import.meta.url, "demo.md");
 const demoText = Deno.readTextFileSync(demo);
 
 Deno.test("mdrb works with stdin", async () => {
-  const { combined } = await $`deno run -A --unstable ${mod} ${demo}`
+  const { combined, stdout, stderr } = await $`deno run -A --unstable ${mod} ${demo}`
     .stdinText(demoText).captureCombined().noThrow();
+
+  // TODO: remove debug logs
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  console.log("mod", mod);
+  console.log("demo", demo);
+  console.log("demoText", demoText);
+  console.log("stdout", stdout);
+  console.log("stderr", stderr);
+  console.log("combined", combined);
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
   const expected = $.dedent`
     step: 1 of 3
