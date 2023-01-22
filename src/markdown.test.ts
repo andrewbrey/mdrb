9c21ef8bb8204474f247b55ca713ff8c8d3029f4
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-22 11:49:20 -0800

    test: add tests

--- src/markdown.test.ts
+++ src/markdown.test.ts
@@ -30,6 +30,11 @@ Deno.test("fileProtocolifyLocalImports works for posix", () => {
     );
 
     assertEquals(
+      fileProtocolifyLocalImports(`import a from '/root/mod.${ext}';`, url),
+      `import a from 'file://a/root/mod.${ext}';`,
+    );
+
+    assertEquals(
       fileProtocolifyLocalImports(`import {abc} from './mod.${ext}';`, url),
       `import {abc} from 'file://a/b/mod.${ext}';`,
     );
@@ -43,6 +48,25 @@ Deno.test("fileProtocolifyLocalImports works for posix", () => {
       fileProtocolifyLocalImports(`import abc, {def, type ghi} from './mod.${ext}';`, url),
       `import abc, {def, type ghi} from 'file://a/b/mod.${ext}';`,
     );
+
+    assertEquals(
+      fileProtocolifyLocalImports(`import abc from './mod.wasm';`, url),
+      `import abc from './mod.wasm';`,
+    );
+
+    assertEquals(
+      fileProtocolifyLocalImports(
+        $.dedent`
+          import * as mod from './mod.${ext}';
+          import * as another from '../another.${ext}';
+        `,
+        url,
+      ),
+      $.dedent`
+        import * as mod from 'file://a/b/mod.${ext}';
+        import * as another from 'file://a/another.${ext}';
+      `,
+    );
   }
 });
 
@@ -53,6 +77,11 @@ Deno.test("fileProtocolifyLocalImports works for windows", () => {
     assertEquals(
       fileProtocolifyLocalImports(`import abc from './mod.${ext}';`, url),
       `import abc from 'file:///D:/a/b/mod.${ext}';`,
+    );
+
+    assertEquals(
+      fileProtocolifyLocalImports(`import abc from '/root/mod.${ext}';`, url),
+      `import abc from 'file:///D:/root/mod.${ext}';`,
     );
 
     assertEquals(
@@ -68,6 +97,25 @@ Deno.test("fileProtocolifyLocalImports works for windows", () => {
     assertEquals(
       fileProtocolifyLocalImports(`import abc, {def, type ghi} from './mod.${ext}';`, url),
       `import abc, {def, type ghi} from 'file:///D:/a/b/mod.${ext}';`,
+    );
+
+    assertEquals(
+      fileProtocolifyLocalImports(`import abc from './mod.wasm';`, url),
+      `import abc from './mod.wasm';`,
+    );
+
+    assertEquals(
+      fileProtocolifyLocalImports(
+        $.dedent`
+          import * as mod from './mod.${ext}';
+          import * as another from '../another.${ext}';
+        `,
+        url,
+      ),
+      $.dedent`
+        import * as mod from 'file:///D:/a/b/mod.${ext}';
+        import * as another from 'file:///D:/a/another.${ext}';
+      `,
     );
   }
 });
