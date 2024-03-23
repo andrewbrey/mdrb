14a97f9108d3452a6339a032dca7abec7d8fa2b2
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-03-23 00:03:08 -0700

    test: windows tests

--- src/util.test.ts
+++ src/util.test.ts
@@ -26,7 +26,7 @@ Deno.test("invariant returns if all ok", () => {
 
 Deno.test("toFileURL works", () => {
 	const toPosix = (path: string) => path.replaceAll(SEP, POSIX_SEP);
-	let cwd = toPosix(Deno.cwd());
+	const cwd = toPosix(Deno.cwd());
 
 	if (Deno.build.os !== "windows") {
 		assertEquals(toFileURL("/a/b/c"), "file:///a/b/c");
@@ -36,13 +36,11 @@ Deno.test("toFileURL works", () => {
 		assertEquals(toFileURL("http://./def.ts"), `file://${cwd}/def.ts`);
 		assertEquals(toFileURL("https://./ghi.ts"), `file://${cwd}/ghi.ts`);
 	} else {
-		cwd = `/${cwd}`;
-
 		assertEquals(toFileURL("/a/b/c"), "file:///D:/a/b/c");
-		assertEquals(toFileURL("./d/e/f"), `file://${cwd}/d/e/f`);
-		assertEquals(toFileURL("../d/e/f"), `file://${toPosix($.path(cwd).join("..").toString())}/d/e/f`);
-		assertEquals(toFileURL("file://./abc.ts"), `file://${cwd}/abc.ts`);
-		assertEquals(toFileURL("http://./def.ts"), `file://${cwd}/def.ts`);
-		assertEquals(toFileURL("https://./ghi.ts"), `file://${cwd}/ghi.ts`);
+		assertEquals(toFileURL("./d/e/f"), `file:///${cwd}/d/e/f`);
+		assertEquals(toFileURL("../d/e/f"), `file:///${toPosix($.path(cwd).join("..").toString())}/d/e/f`);
+		assertEquals(toFileURL("file://./abc.ts"), `file:///${cwd}/abc.ts`);
+		assertEquals(toFileURL("http://./def.ts"), `file:///${cwd}/def.ts`);
+		assertEquals(toFileURL("https://./ghi.ts"), `file:///${cwd}/ghi.ts`);
 	}
 });
