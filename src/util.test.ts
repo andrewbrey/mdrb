6b55b2fa86e00cfba0828774857b87168bd58986
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-03-22 23:59:11 -0700

    test: windows tests

--- src/util.test.ts
+++ src/util.test.ts
@@ -39,7 +39,7 @@ Deno.test("toFileURL works", () => {
 		cwd = `/${cwd}`;
 
 		assertEquals(toFileURL("/a/b/c"), "file:///D:/a/b/c");
-		assertEquals(toFileURL("./d/e/f"), `file:///${cwd}/d/e/f`);
+		assertEquals(toFileURL("./d/e/f"), `file://${cwd}/d/e/f`);
 		assertEquals(toFileURL("../d/e/f"), `file:///${toPosix($.path(cwd).join("..").toString())}/d/e/f`);
 		assertEquals(toFileURL("file://./abc.ts"), `file://${cwd}/abc.ts`);
 		assertEquals(toFileURL("http://./def.ts"), `file://${cwd}/def.ts`);
